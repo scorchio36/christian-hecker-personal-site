c8c5c98539c1bdea5dc7df362a2397acf4d499ad
checker18 <frizzleskillet@yahoo.com>
2017-03-21 18:19:38 -0400

    Add rspec stuff and bootstrap gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
+  get '/home', to: 'static_pages#home'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-  root 'application#hello_test'
-  
+  root 'static_pages#home'
+
 end
