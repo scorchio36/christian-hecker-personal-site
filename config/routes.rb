ac56614ceb7ed6ceb4e7eda0a6b031b8c2bbe197
checker18 <frizzleskillet@yahoo.com>
2017-03-22 18:26:17 -0400

    Add project page

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+
   get '/home', to: 'static_pages#home'
+  get '/projects', to: 'static_pages#projects'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
