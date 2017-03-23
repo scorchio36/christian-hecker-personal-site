d5c99379f9bf39079ea6b8a5d01d25182d4a938a
checker18 <frizzleskillet@yahoo.com>
2017-03-23 12:46:39 -0400

    Set up the contact page

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get '/home', to: 'static_pages#home'
   get '/projects', to: 'static_pages#projects'
   get '/about', to: 'static_pages#about'
+  get '/contact', to: 'static_pages#contact'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
